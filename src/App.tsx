4a6a8de1645a2f8537296f97ffb5a719edd9c810
Omar Ashraf <oashraf304@gmail.com>
2024-04-21 03:28:50 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #444;
+  padding: 20px 25px;
+`;
+
+function App() {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
